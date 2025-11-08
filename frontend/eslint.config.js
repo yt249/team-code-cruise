a9dc3b41c216fc760a8c000fcf810374e63a8377
Christy Tseng <yuchingt@andrew.cmu.edu>
2025-11-08 13:05:19 -0500

    fix linting issue for CI

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -26,4 +26,13 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  {
+    files: ['**/*.test.{js,jsx}', 'tests/**/*.{js,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
 ])
